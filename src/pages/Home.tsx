cf45ecc06d7a94bf977aea4f1b24533cc4c5479d
Igor Nardenio Guerreiro da Silva <igor.guerreiro@fitbank.com.br>
2022-05-31 11:19:18 -0300

    style: criado page home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,14 +3,77 @@ import { useEffect, useState } from 'react';
 
 export default function Home() {
 
+
+    const [valorTotal, setValorTotal] = useState(0);
+
+
     return (
 
-        <div className="row">
-            <nav className="navbar bg-dark row d-flex justify-content-center">
-            
-            </nav>
+        <div className="row d-flex justify-content-center bg-primary">
 
-          <h1>home</h1>
+            <div className="col-6 produtos border">
+                <h5 className="titulo-page text-center mt-3">Produtos</h5>
+
+                <input type="search" name="search_produto" id="search_produto" className="align-self-center" />
+
+                <ul className="list-group">
+                    <li className="bg-dark listagem-produtos list-group-item">
+                        <p>Notebook</p>
+                        <p>R$ 1200.00</p>
+                        <button onClick={() => setValorTotal(valorTotal + 1200.00)} className="btn btn-success">Adicionar</button>
+                    </li>
+                    <li className="bg-dark listagem-produtos list-group-item">
+                        <p>Notebook</p>
+                        <p>R$ 1300.00</p>
+                        <button onClick={() => setValorTotal(valorTotal + 1300.00)} className="btn btn-success">Adicionar</button>
+                    </li>
+                    <li className="bg-dark listagem-produtos list-group-item">
+                        <p>Notebook</p>
+                        <p>R$ 2200.00</p>
+                        <button onClick={() => setValorTotal(valorTotal + 2200.00)} className="btn btn-success">Adicionar</button>
+                    </li>
+
+                    
+                    
+                    
+                   
+
+                </ul>
+            </div>
+
+            <div className="col-6 border">
+                <h5 className="titulo-page text-center mt-3">Produtos Selecionados</h5>
+                <h5 className="titulo-page text-center">Valor do pedido: {valorTotal}</h5>
+                <button className="btn btn-success">Finalizar Pedido</button>
+                <ul className="list-group">
+                <li className="bg-dark listagem-produtos list-group-item">
+                        <p>Notebook</p>
+                        <p>R$ 1200.00</p>
+                        <button className="btn btn-danger">Adicionar</button>
+                    </li>
+                    <li className="bg-dark listagem-produtos list-group-item">
+                        <p>Notebook</p>
+                        <p>R$ 1200.00</p>
+                        <button className="btn btn-danger">Adicionar</button>
+                    </li>
+
+                    <li className="bg-dark listagem-produtos list-group-item">
+                        <p>Notebook</p>
+                        <p>R$ 1200.00</p>
+                        <button className="btn btn-danger">Adicionar</button>
+                    </li>
+
+<li className="bg-dark listagem-produtos list-group-item">
+                        <p>Notebook</p>
+                        <p>R$ 1200.00</p>
+                        <button className="btn btn-danger">Adicionar</button>
+                    </li>
+
+                    
+                   
+
+                </ul>
+            </div>
 
         </div>
     );
